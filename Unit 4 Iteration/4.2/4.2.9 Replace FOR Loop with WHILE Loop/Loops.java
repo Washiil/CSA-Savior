254da58171672e0f6fe86a4ce273be104ebf430e
Washiil <65116802+Washiil@users.noreply.github.com>
2022-10-09 23:58:59 -0400

    4.2.8, 4.2.9

---
+++ Unit 4 Iteration/4.2/4.2.9 Replace FOR Loop with WHILE Loop/Loops.java
@@ -0,0 +1,15 @@
+public class Loops
+{
+    public static void main(String[] args)
+    {
+        // Run this code first to see what it does. 
+        // Then replace the for loop with an equivalent while loop.
+        int x = 0;
+        
+        while (x <= 21)
+        {
+            System.out.println(x);
+            x += 3;
+        }
+    }
+}
