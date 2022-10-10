e369f8a72fd8613ad16b35734f07e0acc2c87a9d
Washiil <65116802+Washiil@users.noreply.github.com>
2022-10-09 22:36:46 -0400

    4.2.6, 4.2.7

---
+++ Unit 4 Iteration/4.2/4.2.6 Print the Odds/Odds.java
@@ -0,0 +1,13 @@
+public class Odds
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0; i <= 100; i +=1)
+    	{
+    	    if (i % 2 != 0)
+    	    {
+		    System.out.println(i);
+    	    }
+    	}
+    }
+}
---
+++ Unit 4 Iteration/4.2/4.2.7 Repeat/Repeat150.java
@@ -0,0 +1,10 @@
+public class Repeat150
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 150; i++)
+        {
+            System.out.println("Are we there yet?");
+        }
+    }
+}
