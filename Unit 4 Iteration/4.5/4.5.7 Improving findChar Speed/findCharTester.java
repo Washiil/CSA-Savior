c882196f76b692e8cfcd299fc235c7118ba8be5a
Washiil <65116802+Washiil@users.noreply.github.com>
2022-10-31 23:07:41 -0400

    4.5 - Unit 4 completed

---
+++ Unit 4 Iteration/4.5/4.5.7 Improving findChar Speed/findCharTester.java
@@ -0,0 +1,18 @@
+public class findCharTester
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        String word = "This is a sentence";
+        String key = "a";
+        System.out.println("The character "+ key+ " is in the String "+ word + ": " + findChar(word, key));
+    }
+    
+    public static boolean findChar(String string, String key)
+    {
+        // Write a more efficient version of isChar than the one in the exercise description
+        // Use the hint!
+        if (string.indexOf(key) == -1) return false;
+        return true;
+    }
+}
