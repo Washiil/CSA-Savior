d373e31806820523b590168c4e24bff89c9dc105
Washi <65116802+Washiil@users.noreply.github.com>
2023-03-27 11:09:23 -0400

    Fix 9.5.9 Code
    Thanks to those that helped!

--- Unit 9 Inheritance/9.5/9.5.9 Assignments/AssignmentRunner.java
+++ Unit 9 Inheritance/9.5/9.5.9 Assignments/AssignmentRunner.java
@@ -1,62 +1,62 @@
-import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.*;
 
 public class AssignmentRunner {
 
     public static void main(String[] args) {
-        // SEE THIS FOR ERROR
-        Scanner strInput = new Scanner(System.in);
-        Scanner dubInput = new Scanner(System.in);
-        Scanner boolInput = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        boolean quit = false;
+        ArrayList assigns = new ArrayList<Assignment>();
         
-        ArrayList<Assignment> assignments = new ArrayList<Assignment>();
-        
-        while(true) {
+        while(true){
             System.out.print("Enter the assignment's name (exit to quit): ");
-            String name = strInput.nextLine();
+            String name = input.nextLine();
             
             if(name.equals("exit")) break;
             
             System.out.print("Enter the due date: ");
-            String dueDate = strInput.nextLine();
+            String date = input.nextLine();
             
             System.out.print("How many points is the assignment worth? ");
-            double totalPoints = dubInput.nextInt();
-            
+            double total_points = input.nextDouble();
             
             System.out.print("How many points were earned? ");
-            double pointsEarned = dubInput.nextDouble();
+            double earned_points = input.nextDouble();
+            
+            input.nextLine();
             
             System.out.print("Is this a (T)est or a (P)roject? ");
-            String type = strInput.nextLine();
+            String assignment_type = input.nextLine();
             
-            if(type.equals("T")) {
+            if(assignment_type.equals("T")){
                 System.out.print("What type of test is it? ");
-                String testType = strInput.nextLine();
-                Test assignment = new Test(name, dueDate, totalPoints, pointsEarned, type);
+                String test_type = input.nextLine();
                 
-                assignments.add(assignment);
-            }else if(type.equals("P")) {
+                Test tes = new Test(name, date, total_points, earned_points, test_type);
+                assigns.add(tes);
+            }
+            else if(assignment_type.equals("P")){
                 System.out.println("Does this project require (true/false) ...");
+                
                 System.out.print("Groups? ");
-                boolean groups = boolInput.nextBoolean();
+                boolean groups = input.nextBoolean();
                 
                 System.out.print("A presentation? ");
-                boolean presentation = boolInput.nextBoolean();
-                Project assignment = new Project(name, dueDate, totalPoints, pointsEarned, groups, presentation);
+                boolean presentation = input.nextBoolean();
                 
-                assignments.add(assignment);
+                input.nextLine();
+                Project proj = new Project(name,date,total_points,earned_points,groups,presentation);
+                assigns.add(proj);
             }
         }
-        
-        for(Assignment test : assignments) {
-            System.out.println(test);
-        }
+        printSummary(assigns);
     }
 
     // Print due date and score percentage on the assignment
     public static void printSummary(ArrayList<Assignment> assignments) {
-       
+       for(int i = 0; i < assignments.size(); i++){
+            double percent = (assignments.get(i).getEarnedPoints()/assignments.get(i).getAvailablePoints())*100; 
+            System.out.println(assignments.get(i).getName() + " - " + percent);
+        }
        
     }
 }
