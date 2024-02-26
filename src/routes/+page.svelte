95ac75412a55acb3b247274df839b37d11dc8bfb
Washi <65116802+Washiil@users.noreply.github.com>
2024-02-25 19:39:24 -0500

    Initial sveltekit commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let count = 0;
+</script>
+
+<h1>Counter: {count}</h1>
+<button on:click={() => count++}>Increase</button>
+<button on:click={() => count--}>Decrease</button>
