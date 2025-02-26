578a031b2413187a7d9cb6dac5b4fc5d823fdcbc
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-26 18:35:43 -0500

    basic layout done

--- src/app/savior/page.tsx
+++ src/app/savior/page.tsx
@@ -1,3 +1,4 @@
+import CodeBlock from "../components/CodeBlock";
 import Sidebar from "../components/Sidebar";
 import Wave from "../components/Wave";
 
@@ -17,13 +18,21 @@ export default function Savior() {
             {/*<Navbar/>*/}
 
             <div className="w-full h-full flex justify-center items-center">
-                <div className="w-[98%] h-[98%] backdrop-blur-xl outline-1 outline outline-zinc-600/45a  rounded-2xl">
-                    <div className="h-full">
-                        <div className="col-span-2">
-                            <h1 className="text-3xl font-mono p-5">CSA-Savior</h1>
+                <div className="w-[98%] h-[98%] backdrop-blur-xl outline-1 outline outline-zinc-600/70 rounded-2xl">
+                    <div className="flex h-full">
+                        <div className="flex flex-col px-4 w-1/3">
+                            <h1 className="text-3xl font-mono py-6 w-full text-center font-bold">CSA-Savior</h1>
+                            <p>Item</p>
                         </div>
 
-                        <div className="w-[1px] bg-white"/>
+                        <div className="w-[1px] h-full bg-gradient-to-b from-transparent via-white to-transparent"/>
+
+                        <div className="p-5 w-full h-full flex flex-col">
+                            <h1 className="text-xl py-3 font-mono">Location/In/File/Structure</h1>
+                            <div className="w-full h-full overflow-y-auto">
+                                <CodeBlock language="javascript">console.log("Hello world!");</CodeBlock>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
