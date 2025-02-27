67c5c9236a65deb92a432e331c7d278168f3a451
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-27 15:07:26 -0500

    Added star button and improved mobile support

--- src/app/savior/page.tsx
+++ src/app/savior/page.tsx
@@ -27,13 +27,25 @@ export default function Savior() {
           <div className="w-[98%] h-[98%] backdrop-blur-xl outline-1 outline outline-zinc-600/70 rounded-2xl">
             <div className="flex flex-col sm:flex-row h-full">
               <div className="flex flex-col px-4 w-full sm:w-1/3 overflow-y-auto">
-                <Link href={"/"}  className="transition-all duration-700 text-3xl font-mono py-6 w-full text-center font-bold cursor-pointer hover:underline">
-                  CSA-Savior
-                </Link>
+                <div className="flex flex-col sm:flex-row justify-between">
+                  <Link
+                    href={"/"}
+                    className="transition-all duration-700 text-3xl font-mono p-2 sm:py-6 text-center font-bold cursor-pointer text-purple-600 sm:text-white sm:hover:underline hover:scale-105"
+                  >
+                    CSA-Savior
+                  </Link>
+                  <Link
+                    href="https://github.com/Washiil/CSA-Savior"
+                    className="font-mono hidden sm:flex justify-center items-center m-4 p-3 transition-all duration-700 outline-1 outline outline-zinc-600/70 text-white rounded-2xl text-md hover:scale-[115%]"
+                  >
+                    Star ⭐
+                  </Link>
+                </div>
+
                 <UnitSelector />
                 <FileTree />
-              </div>
 
+              </div>
               <div className="w-[1px] sm:h-full bg-zinc-600/70" />
                 <div className="p-2 w-full sm:w-2/3 h-full overflow-y-auto">
                   <CodeViewer />
