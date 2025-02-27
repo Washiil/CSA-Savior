18b12389221231ec82fbf85df70b5e036930b513
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-27 09:35:58 -0500

    updated next.config.ts to properly mirror file structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx}', 
   ],
+
   theme: {
     extend: {
       colors: {
