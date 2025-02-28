11cbfc0b1d0838269165adc16e5e691f0a6b705e
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-28 00:07:39 -0500

    updated workflow

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx"],
-  output: "export",
-  basePath: "/nextjs-github-pages",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
