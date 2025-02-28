4953db4525b199bd88a75cf7038248b24713a4c8
Washi <65116802+Washiil@users.noreply.github.com>
2025-02-28 00:13:39 -0500

    deploy please

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["ts", "tsx"],
+  basePath: "/CSA-Savior",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
